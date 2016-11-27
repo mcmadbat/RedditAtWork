07cc79de2ced9a790c050f53ce5c6d65bc9fae89
David Zhang <mcmadbat@gmail.com>
2016-11-27 15:37:46 -0500

    code complete

---
+++ background.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const title = 'all subreddits';
+const hiddenHTML = '<center><h2>NSFW Content Hidden by Reddit At Work :)</h2></center>';
+
+// if on r/all
+if ( document.title === title ){
+
+	// all the divs
+	let divs = $('#siteTable').children().toArray();
+	
+	// filter out to only the nsfw content
+	let nsfw = divs.filter ( div => {
+			return ( $(div).hasClass ('thing') );
+		})
+		.filter ( div => {
+			let n = $(div).children('.entry').children('.flat-list')
+			let firstChild = $(n).children('li')[0];
+
+			// nsfw-stamp marks the nsfw content
+			return ($(firstChild).children(0).hasClass('nsfw-stamp'));
+		});
+
+
+	console.log (`hiding ${nsfw.length} links(s)`);
+
+	// hide the nsfw divs 
+	nsfw.forEach ( toHide => {
+		$(toHide).html(hiddenHTML);
+	});
+}
